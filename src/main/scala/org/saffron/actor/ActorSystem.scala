396c25fa83c4898d85118162c47b6d0af223a051
Pigeon377 <892197240@qq.com>
2022-06-06 23:06:41 +0800

    feat: part of actor system

--- src/main/scala/org/saffron/actor/ActorSystem.scala
+++ src/main/scala/org/saffron/actor/ActorSystem.scala
@@ -1,5 +1,33 @@
 package org.saffron.actor
 
-class ActorSystem {
+import scala.collection.mutable
+
+private class ActorSystem(val name: String) {
+
+  private val actorMap: mutable.HashMap[String, Actor] = new mutable.HashMap[String, Actor]()
+
+  def prop[T <: Actor](path: String, actor: T): Actor = {
+    actorMap.put(path, actor)
+    actor.asInstanceOf[Actor]
+  }
+
+}
+
+
+object ActorSystem {
+
+  private val actorSystemMap: mutable.HashMap[String, ActorSystem] = new mutable.HashMap[String, ActorSystem]()
+
+  def of[T <: Actor](name: String): ActorSystem = {
+    val actorSystemOpt: Option[ActorSystem] = actorSystemMap.get(name)
+    if (actorSystemOpt.isEmpty) {
+      val actorSystem = new ActorSystem(name)
+      actorSystemMap.put(actorSystem.name, actorSystem)
+      actorSystem
+    } else {
+      actorSystemOpt.get
+    }
+  }
+
 
 }
